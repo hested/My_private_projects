25083aecdcac88a74928f90d8cab46aac68858da
Johnni <jhes@itu.dk>
2017-03-04 00:46:18 +0100

    more

--- ShoppingList/app/src/main/java/com/hested/android/shoppinglist/ShoppingActivity.java
+++ ShoppingList/app/src/main/java/com/hested/android/shoppinglist/ShoppingActivity.java
@@ -1,7 +1,10 @@
 package com.hested.android.shoppinglist;
 
+import android.content.pm.PackageInfo;
+import android.content.pm.PackageManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class ShoppingActivity extends AppCompatActivity {
 
@@ -9,5 +12,17 @@ public class ShoppingActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_shopping);
+
+        // Application version number
+        TextView versionNumber = (TextView) findViewById(R.id.version_text_view);
+        String version = "Not available";
+        PackageInfo pInfo;
+        try {
+            pInfo = getPackageManager().getPackageInfo(getPackageName(), 0);
+            version = pInfo.versionName;
+        } catch (PackageManager.NameNotFoundException e) {
+            e.printStackTrace();
+        }
+        versionNumber.append(version);
     }
 }
---
+++ ShoppingList/app/src/main/java/com/hested/android/shoppinglist/ShoppingItem.java
@@ -0,0 +1,35 @@
+package com.hested.android.shoppinglist;
+
+/**
+ * Project:  ShoppingList
+ * Package:  com.hested.android.shoppinglist
+ * Date:     04-03-2017
+ * Time:     00:11
+ * Author:   Johnni Hested
+ */
+
+public class ShoppingItem {
+    private String mItemName;
+    private int mQuantity;
+
+    public ShoppingItem(String itemName, int quantity) {
+        mItemName = itemName;
+        mQuantity = quantity;
+    }
+
+    public String getItemName() {
+        return mItemName;
+    }
+
+    public void setItemName(String itemName) {
+        mItemName = itemName;
+    }
+
+    public int getQuantity() {
+        return mQuantity;
+    }
+
+    public void setQuantity(int quantity) {
+        mQuantity = quantity;
+    }
+}
