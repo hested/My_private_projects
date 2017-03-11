4b8059ec7e61d6ee8918691b5ae9aa4ec6386f57
Johnni <jhes@itu.dk>
2017-03-11 14:09:45 +0100

    new things added

---
+++ CriminalIntent/app/src/main/java/com/hested/android/criminalintent/CrimePagerActivity.java
@@ -0,0 +1,14 @@
+package com.hested.android.criminalintent;
+
+import android.support.v4.app.FragmentActivity;
+
+/**
+ * Project:  CriminalIntent
+ * Package:  com.hested.android.criminalintent
+ * Date:     11-03-2017
+ * Time:     05:33
+ * Author:   Johnni Hested
+ */
+
+public class CrimePagerActivity extends FragmentActivity {
+}
